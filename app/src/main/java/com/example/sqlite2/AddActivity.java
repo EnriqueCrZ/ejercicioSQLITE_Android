a2c734e5fd40affb505cf9e6b804d89a1d474bb4
EnriqueCrZ <enriquecrz21@gmail.com>
2020-10-18 23:39:00 -0600

    Agregar registros a base de datos

--- app/src/main/java/com/example/sqlite2/AddActivity.java
+++ app/src/main/java/com/example/sqlite2/AddActivity.java
@@ -2,13 +2,43 @@ package com.example.sqlite2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddActivity extends AppCompatActivity {
+
+    EditText title_input, author_input, pages_input;
+    Button add_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        title_input = findViewById(R.id.title_input);
+        author_input = findViewById(R.id.author_input);
+        pages_input = findViewById(R.id.pages_inputs);
+        add_button = findViewById(R.id.add_button);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseHelper myDB = new DatabaseHelper(AddActivity.this);
+                long result = myDB.addBook(title_input.getText().toString().trim(),author_input.getText().toString().trim(),Integer.parseInt(pages_input.getText().toString().trim()));
+
+                if(result == -1)
+                    //Toast.makeText(context,"Fallo en agregar registro",Toast.LENGTH_SHORT).show();
+                    title_input.setText("");
+                else{
+                    title_input.setText("");
+                    author_input.setText("");
+                    pages_input.setText("");
+                    //Toast.makeText(context,"Agregado exitosamente",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sqlite2/DatabaseHelper.java
+++ app/src/main/java/com/example/sqlite2/DatabaseHelper.java
@@ -1,12 +1,15 @@
 package com.example.sqlite2;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+    private Context context;
     public static final String DATABASE_NAME = "BookLibrary.db";
     public static final int DATABASE_VERSION = 1;
 
@@ -18,6 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
 
     @Override
@@ -34,5 +38,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
+    }
+
+    long addBook(String title, String author, int pages){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE,title);
+        cv.put(COLUMN_AUTHOR,author);
+        cv.put(COLUMN_PAGES,pages);
+
+        long result = db.insert(TABLE_NAME,null,cv);
+
+        if (result == -1)
+            Toast.makeText(context,"Fallo en agregar registro",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(context,"Agregado exitosamente",Toast.LENGTH_SHORT).show();
+
+        return result;
+
     }
 }
